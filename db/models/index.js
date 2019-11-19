96cbf6c1df45730804ba9293e0f1ad1d5768f5a4
AgusPk <agustinperkins@hotmail.com>
2019-11-19 16:00:54 -0300

    relaciones hechas

--- db/models/index.js
+++ db/models/index.js
@@ -1,5 +1,21 @@
-
 const Tag = require("./Tag");
 const Comment = require("./Comment");
+const User = require("./User");
+const Status = require("./Status");
+const Ticket = require("./Ticket");
 
-module.exports = { Comment, Tag };
+// relacion entre usuarios y preguntas
+User.hasMany(Ticket, { as: "author" });
+Ticket.belongsTo(User, { as: "author" });
+User.hasMany(Ticket, { as: "participant" });
+//status de la pregunta
+Ticket.hasOne(Status);
+//respuestas
+Ticket.hasMany(Comment, { as: "reply" });
+//tags de la pregunta
+Ticket.hasMany(Tag);
+// respuestas de los usuarios
+User.hasMany(Comment, { as: "author" });
+Comment.belongsTo(User, { as: "author" });
+
+module.exports = { User, Tag, Comment, Status, Ticket };
