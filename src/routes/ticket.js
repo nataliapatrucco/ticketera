7ca078265063c1a180b6df7146363b17f19270c0
nataliapatrucco <patrucconatalia@gmail.com>
2019-11-21 18:13:56 -0300

    rutas tag ok

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const {
+  addTag,
+  removeTag,
   fetchTickets,
   fetchStatus,
+  fetchByTitleTag,
   editTicket,
   createTicket,
   deleteTicket
 } = require("../resolvers/ticketResolver");
 
-const { isLoggedIn } = require("../routes/middlewares");
+const { isLoggedIn, isAdmin } = require("../routes/middlewares");
 
 // Traer todos los tickets
 router.get("/", isLoggedIn, fetchTickets);
+
+router.get("/search", isLoggedIn, fetchByTitleTag);
 
 // Crear un ticket
 router.post("/", isLoggedIn, createTicket);
@@ -21,6 +26,12 @@ router.get("/:status", isLoggedIn, fetchStatus);
 
 // Editar un ticket
 router.put("/:ticketId", isLoggedIn, editTicket);
+
+//Agregar tags
+router.put("/:ticketId/addTag/:tagId", isLoggedIn, addTag);
+
+//Quitar tags
+router.put("/:ticketId/removeTag/:tagId", isLoggedIn, removeTag);
 
 // Borrar un ticket
 router.delete("/:id", isLoggedIn, deleteTicket);
