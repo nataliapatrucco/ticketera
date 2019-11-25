3de3e89c66095a190c8eb7d47316e9e64980023d
Natalia <ntl9999@hotmail.com>
2019-11-25 16:04:54 -0300

    redux user

---
+++ src/app/redux/actions/user.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import RECEIVE_USER from "../constants";
+
+const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user
+});
+
+export const loginUser = function({ email, password }) {
+  return function(dispatch, getState) {
+    return axios
+      .post("/api/user/login", { email, password })
+      .then(res => res.data)
+      .then(user => {
+        dispatch(receiveUser(user));
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+};
+
+export const registerUser = ({ username, email, password }) => dispatch =>
+  axios
+    .post("/api/user/register", {
+      name,
+      lastname,
+      email,
+      password
+    })
+    .then(res => res.data)
+    .then(user => {
+      dispatch(receiveUser(user));
+    });
+
+export const logOutUser = () => dispatch =>
+  axios.get("/api/user/logout").then(() => {
+    dispatch(receiveUser({}));
+  });
