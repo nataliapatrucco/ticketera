99f9098ecde33e2bfe7b111c1a5015e45bc87f7f
nataliapatrucco <patrucconatalia@gmail.com>
2019-11-25 17:00:45 -0300

    ok

--- src/app/components/input/style.js
+++ src/app/components/input/style.js
@@ -1,10 +1,21 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
 
-export const Input = styled.input ({
-    borderRadius: "4%",
-    width: "20%",
-    height: "6vh",
-    border: "solid gainsboro 3px"
-})
+export const Input = styled.input({
+  borderRadius: "4%",
+  width: "20%",
+  height: "6vh",
+  border: "solid gainsboro 3px"
+});
 
-
+export const Label = style.p({
+  width: "322.9px",
+  height: "24px",
+  fontFamily: "Montserrat",
+  fontSize: "16px",
+  fontWeight: "normal",
+  fontStretch: "normal",
+  fontStyle: "normal",
+  lineHeight: "1.5",
+  letterSpacing: "0.15px",
+  color: "#8c939f"
+});
