484caa6b70f4ef2bdf49494c515e8dd22216c94b
AgusPk <agustinperkins@hotmail.com>
2019-11-25 16:22:26 -0300

    view v1.0

---
+++ src/app/session/style.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const ViewContainer = styled.div({
+  display: "flex",
+  alignItems: "center"
+});
+
+export const ViewTitle = styled.h1({});
+
+export const ViewText = styled.p({});
