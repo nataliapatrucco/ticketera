6601f48f54fca58e46df8c546c5cd31a69f0263b
nataliapatrucco <patrucconatalia@gmail.com>
2019-11-26 19:53:10 -0300

    open falta fecha

--- src/app/containers/dashboard/index.jsx
+++ src/app/containers/dashboard/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOpen, fetchProcessing } from "../../redux/actions/tickets";
-import Open from "../../components/open";
+import Open from "../../components/Open";
 
 export const Dashboard = () => {
   const dispatch = useDispatch();
