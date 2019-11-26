32bf63a6ad995fc7912bab51d10a537ea486515f
AgusPk <agustinperkins@hotmail.com>
2019-11-26 15:11:30 -0300

    sidebar v1.0

--- src/app/Main/index.js
+++ src/app/Main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Session } from "../session/index";
 import { Switch, Route } from "react-router-dom";
+import Sidebar from "../components/sidebar";
 
 export default () => (
   <div>
