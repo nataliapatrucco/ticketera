a33be2a193ab6792af6afc084223f7158401675b
AgusPk <agustinperkins@hotmail.com>
2019-11-27 16:03:01 -0300

    add & remove participants

--- src/app/components/sidebar/style.js
+++ src/app/components/sidebar/style.js
@@ -1,6 +1,13 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div({});
+export const Container = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "18%",
+  height: "100%",
+  backgroundColor: "#00172e"
+});
 
 export const Header = styled.header({});
 
@@ -8,8 +15,28 @@ export const P5 = styled.img({});
 
 export const Section = styled.section({});
 
-export const TitleContainer = styled.div({});
+export const TitleContainer = styled.div({
+  display: "flex"
+});
 
-export const Icon = styled.img({});
+export const Icon = styled.img({
+  width: "20px",
+  height: "20px",
+  opacity: "0.9",
+  backgroundColor: "#ffffff"
+});
 
-export const Title = styled.h5({});
+export const Title = styled.h5({
+  width: "81px",
+  height: "18px",
+  opacity: "0.9",
+  fontFamily: "Montserrat",
+  fontSize: "14px",
+  fontWeight: "500",
+  fontStretch: "normal",
+  fontStyle: "normal",
+  lineHeight: "normal",
+  letterSpacing: "0.75px",
+  textAlign: "center",
+  color: "#ffffff"
+});
