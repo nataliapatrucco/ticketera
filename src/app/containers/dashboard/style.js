ae146ea4f5adce22f8f6e419c3b60f8f456ca16c
Tativega <tatianavega89@gmail.com>
2019-11-27 00:20:54 -0300

    processing-component

---
+++ src/app/containers/dashboard/style.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+
+export const Dashboard = styled.div({
+    display: 'flex',
+    backgroundColor: '#091e35'
+})
+
+export const Sidebar = styled.div({
+    float: 'right',
+    backgroundColor: '#00172e',
+    width: '18.4%',
+    height: '1961px',
+    display: 'inline-block'
+})
+
+export const Navbar = styled.div({
+   
+})
