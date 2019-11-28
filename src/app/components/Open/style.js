035e8e69542ec15672e17df695f1b1c04c69f9b4
Marce <jmarce_83@hotmail.com>
2019-11-28 18:57:23 -0300

    update

--- src/app/components/Open/style.js
+++ src/app/components/Open/style.js
@@ -193,3 +193,44 @@ export const PartipantsImg = styled.img({
   borderRadius: "50px",
   margin: "10px"
 });
+
+//modal
+
+/* The Modal (background) */
+export const Modal = styled.div({
+  display: "none",
+  position: "fixed",
+  zIndex: "1",
+  paddingTop: "100px",
+  left: "0",
+  top: "0",
+  width: "100%",
+  height: "100%",
+  overflow: "auto",
+  backgroundColor: "rgb(0,0,0)",
+  backgroundColor: "rgba(0,0,0,0.4)"
+});
+
+/* Modal Content */
+export const ModalContent = styled.div({
+  backgroundColor: "#fefefe",
+  margin: "auto",
+  padding: "20px",
+  border: "1px solid #888",
+  width: "80%"
+});
+
+/* The Close Button */
+export const Close = styled.span({
+  color: "#aaaaaa",
+  float: "right",
+  fontSize: "28px",
+  fontWeight: "bold"
+});
+
+// .close:hover,
+// .close:focus {
+//   color: #000;
+//   text-decoration: none;
+//   cursor: pointer;
+// }
