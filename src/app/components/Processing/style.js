1501477c43122886395792ab9c739ad3e9e6f82e
nataliapatrucco <patrucconatalia@gmail.com>
2019-11-29 12:42:09 -0300

    diseno ok

--- src/app/components/Processing/style.js
+++ src/app/components/Processing/style.js
@@ -2,31 +2,41 @@ import styled from "@emotion/styled";
 
 export const Contenedor = styled.div({
   backgroundColor: "#006088",
+  display: "flex",
   height: "201px",
-  width: "300px",
+  flexDirection: "column",
+  marginRight: "50px",
   border: "1px solid #006e9e",
   color: "white",
   borderRadius: "4px"
 });
 
-export const Continue = styled.div({
-  width: "97px",
-  height: "16px",
-  color: "#c4e9f2",
-  fontFamily: "Montserrat",
-  fontSize: "12px",
-  fontWeight: "400",
-  lineHeight: "1.33px",
-  letterSpacing: "0.4px",
-  margin: "0 160px 10px 8px"
-});
-
-export const AlumnContainer = styled.div({
-  padding: "9px 21.2px 9px 8px"
-});
+export const Fotito = styled.img`
+  width: ${props => props.size};
+  height: ${props => props.size};
+  margin: ${props => props.margin};
+  border-radius: 100%;
+  float: left;
+`;
 
 export const AssistantContainer = styled.div({
   margin: "16px 13.4px 0 16px"
+});
+
+export const Assistant = styled.div({
+  fontFamily: "Montserrat",
+  fontWeight: "600",
+  fontSize: "14px",
+  lineHeight: "1.71",
+  letterSpacing: "0.1px",
+  opacity: "90%",
+  width: "234.px",
+  height: "24px"
+});
+
+export const UbicacionContainer = styled.div({
+  margin: "0 19.8px 12px 52.9px"
+  // width: '224px'
 });
 
 export const UbicacionIcon = styled.img({
@@ -42,12 +52,25 @@ export const Ubicacion = styled.div({
   fontSize: "12px",
   width: "214.2px",
   height: "16px",
-  margin: '0',
+  margin: "0",
   letterSpacing: "0.4px",
-  lineHeight: '1.33px',
+  lineHeight: "1.33",
   fontWeight: "normal",
   fontStretch: "normal",
   fontStyle: "normal"
+});
+
+export const AlumnContainer = styled.div({
+  padding: "9px 21.2px 9px 8px"
+});
+
+export const Alumn = styled.div({
+  width: "209.8px",
+  height: "16px",
+  fontFamily: "Montserrat",
+  fontWeight: "600",
+  fontSize: "12px",
+  opacity: "90%"
 });
 
 export const TicketContainer = styled.div({
@@ -64,7 +87,7 @@ export const Ticket = styled.div({
   height: "48px",
   textAlign: "left",
   fontSize: "12px",
-  fontWeight: "normal",
+  fontWeight: "400",
   fontStretch: "normal",
   fontStyle: "normal",
   lineHeight: "16px",
@@ -78,36 +101,16 @@ export const Ticket = styled.div({
   overflow: "hidden"
 });
 
-export const Alumn = styled.div({
-  width: "209.8px",
+export const Continue = styled.div({
+  width: "97px",
   height: "16px",
+  color: "#c4e9f2",
   fontFamily: "Montserrat",
-  fontWeight: '600',
   fontSize: "12px",
-  opacity: "90%"
-});
-
-export const Assistant = styled.div({
-  fontFamily: "Montserrat",
-  fontWeight: '600',
-  fontSize: "14px",
-  lineHeight: "24px",
-  letterSpacing: "0.1px",
-  opacity: "90%",
-  width: "234.px",
-  height: "24px"
-});
-
-export const Fotito = styled.img`
-  width: ${props => props.size};
-  height: ${props => props.size};
-  margin: ${props => props.margin};
-  border-radius: 100%;
-  float: left;
-`;
-
-export const UbicacionContainer = styled.div({
-  margin: "0 19.8px 12px 52.9px"
+  fontWeight: "400",
+  lineHeight: "1.33px",
+  letterSpacing: "0.4px",
+  margin: "0 160px 10px 8px"
 });
 
 // export const Ubicacion = styled.p({
--- src/app/components/sidebar/style.js
+++ src/app/components/sidebar/style.js
@@ -7,50 +7,67 @@ export const Container = styled.div({
   // textAlign: "center",
   height: "100%",
   backgroundColor: "#00172e",
-  marginLeft: "25%"
+  //marginLeft: "25%"
 });
 
 export const Header = styled.header({
-  // display: "inline-block",
-  height: "0.5%",
-  // alignContent: "center",
-  // marginLeft: "50px",
-  // marginRight: "50px",
-  marginTop: "30px",
-  textAlign: "center"
-});
+  width: '53.8%',
+  height: '35.8px',
+  margin: '22.2px 32px 74.7px 75px'
+//   display: "inline-block",
+//   height: "0.5%",
+//   alignContent: "center",
+//   marginLeft: "50px",
+//   marginRight: "50px",
+//   marginTop: "30px",
+//   textAlign: "center"
+ });
+
 
 export const P5 = styled.img({
-  width: "150px",
-  textAlign: "center"
+  width: "100%",
+  height: '100%'
+  //textAlign: "center"
 });
 
-export const Section = styled.section({
-  display: "flex",
-  textAlign: "center",
-  flexDirection: "column",
-  // justifyContent: "center",
-  width: "100%", 
-  marginTop: '66px'
-});
+// export const Section = styled.section({
+//   display: "flex",
+//   //textAlign: "center",
+//   flexDirection: "column",
+//   // justifyContent: "center",
+//   width: "100%", 
+//   //marginTop: '66px'
+// });
 
 export const TitleContainer = styled.div({
   display: "flex"
 });
 
+export const IconContainer = styled.div({
+  width: '20px',
+  height: '20px',
+  opacity: "90%",
+  float: 'left',
+  marginBottom: '37px',
+  marginLeft: '77px',
+  
+})
+
 export const Icon = styled.img({
-  // opacity: "90%",
+  width: '100%',
+  height: '100%',
   // border: "solid 1px #ffffff",
   // borderRadius: "30px"
 });
 
-export const Title = styled.h5({
+export const Title = styled.div({
   opacity: "90%",
   fontFamily: "Montserrat",
   fontSize: "14px",
   fontWeight: "500",
   letterSpacing: "0.75px",
-  textAlign: "center",
+  //textAlign: "center",
   color: "#ffffff",
-  textDecoration: "none"
+  textDecoration: "none",
+  margin: '1px 0 39px 10px'
 });
