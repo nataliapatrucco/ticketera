e0d29a0e3be0290889401559632306d9381a0b92
Tativega <tatianavega89@gmail.com>
2019-12-02 21:44:30 -0300

    user-tickets-buttons

--- src/app/components/UserTickets/index.jsx
+++ src/app/components/UserTickets/index.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUserTickets } from "../../redux/actions/tickets";
 import { QuestionStatus, QuestionStatusContainer } from './style'
 
-export default () => {
+export default ({match}) => {
   const dispatch = useDispatch();
+  const [value, setValue] = useState("pendiente")
 
   useEffect(() => {
     dispatch(fetchUserTickets());
@@ -14,16 +16,21 @@ export default () => {
 
   return (
     <QuestionStatusContainer>
-    <QuestionStatus>
+      <Link to ={`${match.url}/pending`}>
+    <QuestionStatus value={value} >
       PENDIENTES
     </QuestionStatus>
-    <QuestionStatus>
+    </Link>
+    <Link to ={`${match.url}/answered`}>
+    <QuestionStatus value={value} >
       RESPONDIDAS
     </QuestionStatus>
-    <QuestionStatus>
+    </Link>
+    <Link to={`${match.url}/rejected`}>
+    <QuestionStatus value={value} >
       RECHAZADAS
     </QuestionStatus>
-
+    </Link>
   </QuestionStatusContainer>
   ) 
 };
