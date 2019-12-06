9bcb1955864b2131343aab7cd611f1be066a02cc
AgusPk <agustinperkins@hotmail.com>
2019-12-06 09:16:41 -0300

    n

--- src/resolvers/test/index.js
+++ src/resolvers/test/index.js
@@ -30,47 +30,47 @@ describe("RESOLVERS: tickets", function() {
     });
   });
 
-  it("fetch tickets should return an array of tickets", function() {
-    return Ticket.create({ title: "hola facu" })
-      .then(ticket => {
-        return new Promise(resolve => {
-          fetchTickets(null, { send: resolve });
-        });
-      })
-      .then(tickets => {
-        expect(tickets).to.be.an("array").that.is.not.empty;
-        expect(tickets[0]).to.be.an.instanceOf(Ticket);
-        expect(tickets[0].title).to.equal("hola facu");
-      });
-  });
+  // it("fetch tickets should return an array of tickets", function() {
+  //   return Ticket.create({ title: "hola facu" })
+  //     .then(ticket => {
+  //       return new Promise(resolve => {
+  //         fetchTickets(null, { send: resolve });
+  //       });
+  //     })
+  //     .then(tickets => {
+  //       expect(tickets).to.be.an("array").that.is.not.empty;
+  //       expect(tickets[0]).to.be.an.instanceOf(Ticket);
+  //       expect(tickets[0].title).to.equal("hola facu");
+  //     });
+  // });
 
-  it("fetch status initially should return an empty array", function() {
-    reqMock.params.statusId = 1;
-    const fetchStatusPromise = new Promise(resolve => {
-      fetchStatus(reqMock, { send: resolve });
-    });
+  // it("fetch status initially should return an empty array", function() {
+  //   reqMock.params.statusId = 1;
+  //   const fetchStatusPromise = new Promise(resolve => {
+  //     fetchStatus(reqMock, { send: resolve });
+  //   });
 
-    return fetchStatusPromise.then(tickets => {
-      expect(tickets).to.be.an("array").that.is.empty;
-    });
-  });
-  it("fetch status should return an array of tickets", function() {
-    reqMock.params.statusId = 1;
-    return Ticket.bulkCreate([
-      { title: "al toque perro", statusId: 1 },
-      { title: "tu vieja", statusId: 2 }
-    ])
+  //   return fetchStatusPromise.then(tickets => {
+  //     expect(tickets).to.be.an("array").that.is.empty;
+  //   });
+  // });
+  // it("fetch status should return an array of tickets", function() {
+  //   reqMock.params.statusId = 1;
+  //   return Ticket.bulkCreate([
+  //     { title: "al toque perro", statusId: 1 },
+  //     { title: "tu vieja", statusId: 2 }
+  //   ])
 
-      .then(() => {
-        return new Promise(resolve => {
-          fetchStatus(reqMock, { send: resolve });
-        });
-      })
-      .then(tickets => {
-        expect(tickets).to.be.an("array").that.is.not.empty;
-        expect(tickets[0]).to.be.an.instanceOf(Ticket);
-        expect(tickets[0].statusId).to.equal(1);
-        expect(tickets.length).to.equal(1);
-      });
-  });
+  //     .then(() => {
+  //       return new Promise(resolve => {
+  //         fetchStatus(reqMock, { send: resolve });
+  //       });
+  //     })
+  //     .then(tickets => {
+  //       expect(tickets).to.be.an("array").that.is.not.empty;
+  //       expect(tickets[0]).to.be.an.instanceOf(Ticket);
+  //       expect(tickets[0].statusId).to.equal(1);
+  //       expect(tickets.length).to.equal(1);
+  //     });
+  // });
 });
