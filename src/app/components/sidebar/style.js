c2653a40d612209e93e19b4b2e420643b57e352d
Tativega <tatianavega89@gmail.com>
2019-12-12 13:58:38 -0300

    ajustes modal perfil

--- src/app/components/sidebar/style.js
+++ src/app/components/sidebar/style.js
@@ -3,41 +3,21 @@ import styled from "@emotion/styled";
 export const Container = styled.div({
   display: "flex",
   flexDirection: "column",
-  // alignContent: "center",
-  // textAlign: "center",
   height: "100%",
   backgroundColor: "#00172e",
-  //marginLeft: "25%"
 });
 
 export const Header = styled.header({
   width: '53.8%',
   height: '35.8px',
   margin: '22.2px 32px 74.7px 75px'
-//   display: "inline-block",
-//   height: "0.5%",
-//   alignContent: "center",
-//   marginLeft: "50px",
-//   marginRight: "50px",
-//   marginTop: "30px",
-//   textAlign: "center"
  });
 
 
 export const P5 = styled.img({
   width: "100%",
   height: '100%'
-  //textAlign: "center"
 });
-
-// export const Section = styled.section({
-//   display: "flex",
-//   //textAlign: "center",
-//   flexDirection: "column",
-//   // justifyContent: "center",
-//   width: "100%", 
-//   //marginTop: '66px'
-// });
 
 export const TitleContainer = styled.div({
   display: "flex"
@@ -56,8 +36,6 @@ export const IconContainer = styled.div({
 export const Icon = styled.img({
   width: '100%',
   height: '100%',
-  // border: "solid 1px #ffffff",
-  // borderRadius: "30px"
 });
 
 export const Title = styled.div({
@@ -66,7 +44,6 @@ export const Title = styled.div({
   fontSize: "14px",
   fontWeight: "500",
   letterSpacing: "0.75px",
-  //textAlign: "center",
   color: "#ffffff",
   textDecoration: "none",
   margin: '1px 0 39px 10px'
