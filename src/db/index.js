0ef70e773467f3bfb3723ef77959e98c7c644663
facu velasco <velascofacundo@gmail.com>
2019-12-12 12:19:13 -0300

    feat: docker containers config

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,9 @@
 const S = require("sequelize");
-const db = new S("postgres://localhost:5432/ticketera", { logging: false });
+const { DB_PASS, DB_USER, DB_NAME, DB_HOST } = process.env;
+const db = new S(DB_NAME, DB_USER, DB_PASS, {
+  dialect: "postgres",
+  host: DB_HOST,
+  port: 5432,
+  logging: false
+});
 module.exports = db;
