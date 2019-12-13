4971ea0f6626d9655dbe64da12d9e84ee71eebaf
Ubuntu <ubuntu@ip-172-31-6-7.sa-east-1.compute.internal>
2019-12-13 18:24:11 +0000

    seed with super user

--- seed.js
+++ seed.js
@@ -1,14 +1,16 @@
-const { Status } = require("./src/db/models/index");
+const { User, Status } = require("./src/db/models/index");
 
 const promises = [
   Status.create({ state: "open" }),
   Status.create({ state: "process" }),
   Status.create({ state: "close" }),
   Status.create({ state: "rejected" })
+  User.create({ name: 'super', lastname: 'admin', email: 'contacto@plataforma5.la', isAdmin: 1, password: 'ticketerawashere' })
 ];
 
 Promise.all(promises).then(statusList => {
-  statusList.forEach(status => {});
+  statusList.forEach(status => console.log(status));
 
   process.exit();
 });
+
